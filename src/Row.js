39fdea2775360321b37386703ef9549d53e8b3e1
Akinkunmi <akinkunmiisrael41@gmail.com>
2020-11-19 01:40:04 +0000

    new commit

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-//import axios from "./axios";
+import Row from './Row.css';
 import axios from 'axios';
 
 const getImg = 'https://image.tmdb.org/t/p/original/';
