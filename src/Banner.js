8b11c8753d92b49bfc3cb694abbd70006a74ef02
Akinkunmi <akinkunmiisrael41@gmail.com>
2020-11-19 16:29:17 +0000

    added Banner.js

---
+++ src/Banner.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+function Banner() {
+    return (
+        const [movie, setMovie] = useState([]);
+
+        useEffect(() => {
+            
+            }
+        }, [])
+
+        <header>
+
+
+            
+        </header>
+    )
+}
+
+export default Banner
